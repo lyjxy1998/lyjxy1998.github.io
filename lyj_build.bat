e2bd9298dbbf5a4222a1f2a83376f7bcd85e0723
lyjxy <lai@ripsim.com>
2023-12-29 18:30:36 -0500

    Init app

---
+++ lyj_build.bat
@@ -0,0 +1,4 @@
+ng build --configuration production
+
+pause
+
---
+++ lyj_push.bat
@@ -0,0 +1,7 @@
+git add . 
+git commit -m "Init app"
+git push 
+
+pause
+
+@REM Navigate to the settings for your repository or enter the url https://github.com/username/username.github.io/settings
